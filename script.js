01c470c70b38b3247a4b21f4e0797cb70da5e141
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-30 17:06:10 +0330

    0.5
    logic revamp
    (12
    +
    12)

--- script.js
+++ script.js
@@ -3,20 +3,27 @@ const input = document.querySelectorAll('.buttons button')
 let operand1 = '';
 let operand2 = '';
 let operator = '';
-let currentValue = '';
+let currentValue2 = '';
+let currentValue1 = '';
+let result = '';
 
 input.forEach(button => {
     button.addEventListener("click", (e) => {
         target = e.target.textContent;
         if (target === "Clear") { clearButton() }
-        else if (target === "=") { 
-            equationButton(currentValue)
-            currentValue = "";
+        else if (target === "=") { equationButton() }
+        else if (display.textContent === "Infinity" || display.textContent === "NaN") {
+            currentValue2 = "";
+            display.textContent = "";
+        }
+        else if (target === "+" || target === "-" || target === "*" || target === "/") {
+            opeButtons(currentValue2);
+            operator = e.target.textContent;
+            currentValue2 = "";
         }
         else {
-            currentValue += e.target.textContent;
-            display.textContent = currentValue;
-
+            currentValue2 += e.target.textContent;
+            display.textContent = currentValue2;
         };
     })
 })
@@ -37,6 +44,9 @@ function divide(num1, num2) {
 }
 
 function operate(operator, number1, number2) {
+    console.log(operand1);
+    console.log(operand2);
+    console.log(operator);
     switch (operator) {
         case "+":
             return add(number1, number2);
@@ -46,37 +56,54 @@ function operate(operator, number1, number2) {
             return multiply(number1, number2);
         case "/":
             return divide(number1, number2);
-    }
+    };
+
 
 }
 
 
 function clearButton() {
-    currentValue = "";
+    currentValue2 = "";
     display.textContent = "";
 }
-function equationButton(currentValue) {
-    console.log(currentValue.split(""));
-    for (let i = 0; i < currentValue.length; i++) {
-        if (currentValue[i] == "-" || currentValue[i] == "+" || currentValue[i] == "*" || currentValue[i] == "/") {
-            operator = currentValue[i];
-            let newValue1 = [];
-            let newValue2 = [];
+function equationButton() {
+    // this code is for parsing the whole string, its harder and more complex
+    // I accidentally did this first time and it works :P
 
-            for (let j = 0; j < i; j++) {
-                newValue1 += currentValue[j];
-            }
-            operand1 = Number(newValue1);
+    // for (let i = 0; i < currentValue2.length; i++) {
+    //     if (currentValue2[i] == "-" || currentValue2[i] == "+" || currentValue2[i] == "*" || currentValue2[i] == "/") {
+    //         operator = currentValue2[i];
+    //         let newValue1 = [];
+    //         let newValue2 = [];
 
-            let j = i + 1;
-            for (j; j < currentValue.length; j++) {
-                newValue2 += currentValue[j];
-            }
-            operand2 = Number(newValue2);
+    //         for (let j = 0; j < i; j++) {
+    //             newValue1 += currentValue2[j];
+    //         }
+    //         operand1 = Number(newValue1);
 
-            
-            display.textContent = operate(operator, operand1, operand2);
+    //         let j = i + 1;
+    //         for (j; j < currentValue2.length; j++) {
+    //             newValue2 += currentValue2[j];
+    //         }
+    //         operand2 = Number(newValue2);
 
-        }
-    }
+
+    //         result = operate(operator, operand1, operand2);
+    //         display.textContent = result;
+    //         return currentValue2 = result;
+
+    //     }
+    // }
+    operand1 = Number(currentValue1);
+    operand2 = Number(currentValue2);
+    result = operate(operator, operand1, operand2);
+    display.textContent = result;
+    currentValue1 = "";
+    currentValue2 = ""; 
+    
+}
+function opeButtons(currentValue2){
+    operand1 = currentValue2;
+    display.textContent = "";
+    return currentValue1 = operand1;
 }
